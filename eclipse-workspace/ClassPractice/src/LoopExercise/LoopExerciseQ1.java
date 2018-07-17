98220bc8c818fede997691304269ff14e86ed5af
th3pi <38303217+th3pi@users.noreply.github.com>
2018-07-17 15:42:39 -0400

    first commit

---
+++ eclipse-workspace/ClassPractice/src/LoopExercise/LoopExerciseQ1.java
@@ -0,0 +1,17 @@
+package LoopExercise;
+
+public class LoopExerciseQ1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 1; i <= 100; i++) {
+			if(i % 3 == 0)
+				System.out.println("Fizz");
+			else if(i % 5 == 0)
+				System.out.println("BuzzFizz");
+			else
+				System.out.println(i);
+		}
+	}
+
+}
